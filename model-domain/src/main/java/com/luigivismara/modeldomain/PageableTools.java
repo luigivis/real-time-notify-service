edd3219bd1ab9608f6ea4134a0d5f3b58636e98a
Luigi Vismara <33238929+luigivis@users.noreply.github.com>
2024-09-08 22:04:26 -0600

    Implement pagination utility in PageableTools class with HttpResponse handling

--- model-domain/src/main/java/com/luigivismara/modeldomain/PageableTools.java
+++ model-domain/src/main/java/com/luigivismara/modeldomain/PageableTools.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
